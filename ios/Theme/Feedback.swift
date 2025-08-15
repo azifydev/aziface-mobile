48029355d1fa0d20c9a00904622b0cbd78ff6b01
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-08-15 11:53:13 -0300

    feat(ios): add `Feedback` class to manager feedbacks

---
+++ ios/Theme/Feedback.swift
@@ -0,0 +1,23 @@
+//
+//  Feedback.swift
+//  Pods
+//
+//  Created by Daniel Sansão Araldi on 15/08/25.
+//
+
+public class Feedback: CommonStyle {
+  private static let KEY: String = "feedback"
+  private let theme: NSDictionary?
+  private let color: Color
+  
+  init() {
+    self.theme = Style().getTarget(Feedback.KEY)
+    self.color = Color()
+    
+    super.init(key: Feedback.KEY)
+  }
+  
+  public func getTextColor() -> UIColor {
+    return self.color.getColor(self.theme, key: "textColor");
+  }
+}
