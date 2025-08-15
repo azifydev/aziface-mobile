26149d58681449e8d2a139d55e669d1e13fc004a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-08-15 13:16:24 -0300

    feat(ios): add `RetryScreen` class and add it in the `Guidance` class

--- ios/Theme/Guidance.swift
+++ ios/Theme/Guidance.swift
@@ -8,17 +8,23 @@
 public class Guidance: CommonStyle {
   private static let KEY: String = "guidance"
   private let button: Button
+  private let retryScreen: RetryScreen
   
   init() {
     let theme = Style().getTarget(Guidance.KEY)
     
     self.button = Button(theme: theme)
+    self.retryScreen = RetryScreen(theme: theme)
     
     super.init(key: Guidance.KEY)
   }
   
   public func getButton() -> Button {
     return self.button
+  }
+  
+  public func getRetryScreen() -> RetryScreen {
+    return self.retryScreen
   }
   
   override public func getForegroundColor() -> UIColor {
