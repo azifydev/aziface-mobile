c72e685e3284ce3a9071accf3a23b2255e7706e1
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-08-15 13:50:56 -0300

    feat(ios): add `ReviewScreen` class

---
+++ ios/Theme/ReviewScreen.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewScreen.swift
+//  Pods
+//
+//  Created by Daniel Sansão Araldi on 15/08/25.
+//
+
+public class ReviewScreen: CommonViewStyle {
+  
+  init(theme: NSDictionary?) {
+    super.init(theme: theme, key: "reviewScreen")
+  }
+}
